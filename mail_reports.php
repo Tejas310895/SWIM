c87e65e691885844c39726274a8dcc0721eb9538
tshirsat700@gmail.com <tshirsat700@gmail.com>
2020-10-19 13:32:23 +0530

    mail send

---
+++ mail_reports.php
@@ -0,0 +1,34 @@
+<?php 
+
+include ("includes/db.php");
+
+$to = 'tshirsat700@gmail.com';
+$subject = 'Daily Production Reports';
+$from = 'tshirsat700@gmail.com';
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+$message = '<html><body>';
+$message = 
+
+$get_raw_stock = "select * from raw_items";
+$run_raw_stock = mysqli_query($con,$get_raw_stock);
+while($row_raw_stock = mysqli_fetch_array()){
+
+    $item_name = $row_raw_stock['item_name'];
+    $item_unit = $row_raw_stock['item_unit'];
+    $item_stock = $row_raw_stock['item_stock'];
+
+    echo "
+        <h5>$item_name - $item_stock $item_unit</h5>
+    ";
+
+}
+;
+
+$message .= '</body></html>';
+
+mail($to, $subject, $message, $headers);
+
+?>
