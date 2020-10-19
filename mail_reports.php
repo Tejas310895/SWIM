58db79b6478813936a1df966887a469baac3b64b
tshirsat700@gmail.com <tshirsat700@gmail.com>
2020-10-19 13:46:59 +0530

    error

--- mail_reports.php
+++ mail_reports.php
@@ -1,5 +1,20 @@
 <?php 
 
+function raw_inventory(){
+    $get_raw_stock = 'select * from raw_items';
+    $run_raw_stock = mysqli_query($con,$get_raw_stock);
+    while($row_raw_stock = mysqli_fetch_array()){
+
+        $item_name = $row_raw_stock['item_name'];
+        $item_unit = $row_raw_stock['item_unit'];
+        $item_stock = $row_raw_stock['item_stock'];
+
+        return '<h5>$item_name - $item_stock $item_unit</h5>';
+
+    }
+
+}
+
 include ("includes/db.php");
 
 $to = 'tshirsat700@gmail.com';
@@ -10,21 +25,7 @@ $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 $message = '<html><body>';
-$message .=
-
-$get_raw_stock = 'select * from raw_items';
-$run_raw_stock = mysqli_query($con,$get_raw_stock);
-while($row_raw_stock = mysqli_fetch_array()){
-
-    $item_name = $row_raw_stock['item_name'];
-    $item_unit = $row_raw_stock['item_unit'];
-    $item_stock = $row_raw_stock['item_stock'];
-
-    echo '
-        <h5>$item_name - $item_stock $item_unit</h5>
-    ';
-
-};
+$message .= raw_inventory();
 
 $message .= '</body></html>';
 
