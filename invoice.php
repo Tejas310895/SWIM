e1bf55b8da4315719d594752c979ac8055bc87b6
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-09-17 18:26:55 +0530

    error

--- invoice.php
+++ invoice.php
@@ -61,21 +61,21 @@ if(isset($_GET['invoice_no'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SW INC <?php echo $invoice_no; ?></title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="http://localhost/SWIM/assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="http://localhost/SWIM/assets/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="http://localhost/SWIM/assets/vendors/jvectormap/jquery-jvectormap.css">
-    <link rel="stylesheet" href="http://localhost/SWIM/assets/vendors/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="http://localhost/SWIM/assets/vendors/owl-carousel-2/owl.carousel.min.css">
-    <link rel="stylesheet" href="http://localhost/SWIM/assets/vendors/owl-carousel-2/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/vendors/jvectormap/jquery-jvectormap.css">
+    <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="assets/vendors/owl-carousel-2/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/vendors/owl-carousel-2/owl.theme.default.min.css">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <!-- endinject -->
     <!-- Layout styles -->
-    <link rel="stylesheet" href="http://localhost/SWIM/assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <!-- End layout styles -->
-    <link rel="shortcut icon" href="http://localhost/SWIM/assets/images/favicon.png" />
+    <link rel="shortcut icon" href="assets/images/favicon.png" />
     <!-- <script>
         window.onload = function () {
             window.print();
