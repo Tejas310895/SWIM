71d5d978240e94e50f7ff9876a166af94f343129
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2023-12-19 23:35:20 +0530

    changes

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
-if ( $_SERVER['HTTP_HOST'] == 'localhost' ){
-    $con = mysqli_connect('localhost','root','','swaf');
-}else{
-    $con = mysqli_connect('localhost','u708087849_swaf','Silverwrap@11','u708087849_swaf');
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $con = mysqli_connect('localhost', 'root', '', 'swaf');
+} else {
+    $con = mysqli_connect('localhost', 'u708087849_swaf', 'Silverwrap@11', 'u708087849_swaf');
 }
 
 
