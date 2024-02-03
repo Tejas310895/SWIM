60fec50c0d87dd20c3001afcdfd78e450ba5b73b
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-02-03 22:55:36 +0530

    changes

--- js/datatable.js
+++ js/datatable.js
@@ -1,10 +1,29 @@
 $(document).ready(function () {
 	document.title = 'Summary';
+	$('#example').DataTable({
+		"dom": "<'col-sm-12 col-md-1 btn btn-primary btn-sm my-2'B><'col-sm-12 col-md-10'f><'col-sm-12 col-md-1 dataright'l>" + "<'col-sm-12 col-md-12 px-0'tr>" + "<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 pull-right'p>",
+		"paging": true,
+		"pagingType": "simple",
+		"responsive": true,
+		"autoWidth": true,
+		"buttons": [
+			{
+				extend: 'excelHtml5',
+				text: 'Excel',
+				customize: function (xlsx) {
+					var source = xlsx.xl['workbook.xml'].getElementsByTagName('sheet')[0];
+					source.setAttribute('name', 'New Name');
+				}
+			}
+		]
+
+	});
 	$('#invoice_bulk').DataTable({
 		"paging": true,
 		"pagingType": "simple",
 		"responsive": true,
 		"autoWidth": true,
+		"serverSide": true,
 		"ajax": {
 			url: 'ajaxphp/ajaxinvoice.php',
 			type: 'post',
@@ -40,7 +59,8 @@ $(document).ready(function () {
 		"pagingType": "simple",
 		"responsive": true,
 		"autoWidth": true,
-		ajax: {
+		"serverSide": true,
+		"ajax": {
 			url: 'ajaxphp/ajaxinvoice.php',
 			type: 'post',
 			data: { 'invoice_table': 'invoice_table' },
