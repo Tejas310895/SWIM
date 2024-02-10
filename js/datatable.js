dfacbf4ef384a24b06015ed6a2793c24f1ffef81
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-02-10 15:14:12 +0530

    changes

--- js/datatable.js
+++ js/datatable.js
@@ -19,6 +19,7 @@ $(document).ready(function () {
 
 	});
 	$('#invoice_bulk').DataTable({
+		"dom": "<'col-sm-12 col-md-1 btn btn-primary btn-sm my-2'B><'col-sm-12 col-md-10'f><'col-sm-12 col-md-1 dataright'l>" + "<'col-sm-12 col-md-12 px-0'tr>" + "<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 pull-right'p>",
 		"paging": true,
 		"pagingType": "simple",
 		"responsive": true,
@@ -41,20 +42,21 @@ $(document).ready(function () {
 			{ "data": "balance" },
 			{ "data": "status" },
 			{ "data": "action" },
+		],
+		"buttons": [
+			{
+				extend: 'excelHtml5',
+				text: 'Excel',
+				customize: function (xlsx) {
+					var source = xlsx.xl['workbook.xml'].getElementsByTagName('sheet')[0];
+					source.setAttribute('name', 'New Name');
+				}
+			}
 		]
-		// "buttons": [
-		// 		{
-		// extend: 'excelHtml5',
-		// text: 'Excel',
-		// customize: function( xlsx ) {
-		//   var source = xlsx.xl['workbook.xml'].getElementsByTagName('sheet')[0];
-		//   source.setAttribute('name','New Name');
-		// 			}
-		// 		}
-		// ] 
 
 	});
 	$('#invoice').DataTable({
+		"dom": "<'col-sm-12 col-md-1 btn btn-primary btn-sm my-2'B><'col-sm-12 col-md-10'f><'col-sm-12 col-md-1 dataright'l>" + "<'col-sm-12 col-md-12 px-0'tr>" + "<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 pull-right'p>",
 		"paging": true,
 		"pagingType": "simple",
 		"responsive": true,
@@ -71,17 +73,17 @@ $(document).ready(function () {
 			{ "data": "company_details" },
 			{ "data": "cust_details" },
 			{ "data": "action" },
+		],
+		"buttons": [
+			{
+				extend: 'excelHtml5',
+				text: 'Excel',
+				customize: function (xlsx) {
+					var source = xlsx.xl['workbook.xml'].getElementsByTagName('sheet')[0];
+					source.setAttribute('name', 'New Name');
+				}
+			}
 		]
-		// "buttons": [
-		// 		{
-		// extend: 'excelHtml5',
-		// text: 'Excel',
-		// customize: function( xlsx ) {
-		//   var source = xlsx.xl['workbook.xml'].getElementsByTagName('sheet')[0];
-		//   source.setAttribute('name','New Name');
-		// 			}
-		// 		}
-		// ] 
 
 	});
 });
